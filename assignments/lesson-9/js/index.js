518718ff92be1e5589891d00246720c91a95e268
Mariana <mariana@funks-mac-pro-4.local>
2019-03-10 01:29:10 -0500

    load data

--- assignments/lesson-9/js/index.js
+++ assignments/lesson-9/js/index.js
@@ -1,1 +1,34 @@
 // load json into index.html
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+var request = new XMLHttpRequest();
+
+request.open('GET', requestURL);
+
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+    var towns = request.response;
+    populateTowns(towns);
+    // populateEvents(towns);
+}
+
+function populateTowns(jsonObj) {
+    // console.log(jsonObj)
+
+    var currTowns = ['Preston', 'Soda-Springs', 'Fish-Haven']
+    for (let i = 0; i < jsonObj.towns.length; i++) {
+        var town = jsonObj.towns[i];
+        var townName = town.name.replace(' ', '-')
+        if (currTowns.indexOf(townName) != -1) {
+            // console.log(town)
+            document.getElementById(townName + "-town").innerText = town.name
+            document.getElementById(townName + "-motto").innerHTML = `<strong><em>`+town.motto+`</em></strong>`
+            document.getElementById(townName + "-yearFounded").innerHTML = `<strong>Year Founded: </strong>`+town.yearFounded
+            document.getElementById(townName + "-currentPopulation").innerHTML = `<p><strong>Population: </strong>`+town.currentPopulation
+            document.getElementById(townName + "-averageRainfall").innerHTML = `<strong>Annual Rain Fall: </strong>`+town.averageRainfall+`"`
+        }
+
+    }
+}
